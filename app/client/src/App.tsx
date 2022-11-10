688639bf1ffc7a10831d353ac41a37b062c715ed
isaaclee12 <isaac.lee@uvm.edu>
2022-11-10 14:40:19 -0500

    App prototype works for the read end. Now to implement the Create, Update, and Delete parts.

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -23,24 +23,68 @@ function App() {
   const [shifts, setShifts] = useState([])
   const [dates, setDates] = useState([])
 
-  // useEffect(() => {
-  //   console.log("shifts:", shifts);
-  // }, [shifts]);
+
+  const [mondayShifts, setMondayShifts] = useState([])
+  const [tuesdayShifts, setTuesdayShifts] = useState([])
+  const [wednesdayShifts, setWednesdayShifts] = useState([])
+  const [thursdayShifts, setThursdayShifts] = useState([])
+  const [fridayShifts, setFridayShifts] = useState([])
+  const [saturdayShifts, setSaturdayShifts] = useState([])
+  const [sundayShifts, setSundayShifts] = useState([])
+
+  useEffect(() => {
+    console.log("shifts:", mondayShifts);
+  }, [mondayShifts]);
   
   useEffect(() => {
 
-    function fetchData(endpoint: string): any {
+    function fetchData(endpointName: string): any {
+
+      // add endpoint to string
+      let endpoint = "http://localhost:8000/schedule/" + endpointName +  "/";
+
       fetch(endpoint, {
         method: "GET",
         mode: 'cors'
       })
       .then(response => response.json()
         .then(data => {
-            if (endpoint == "http://localhost:8000/schedule/shifts/") {
-              console.log("Got following data from endpoint", endpoint, ":", data);
-              setShifts(data);
-            } else {
-              setDates(data);
+            switch (endpointName) {
+              case "shifts":
+                setShifts(data);
+                break;
+ 
+              case "dates":
+                setDates(data);
+                break;
+ 
+              case "monday":
+                setMondayShifts(data);
+                break;
+ 
+              case "tuesday":
+                setTuesdayShifts(data);
+                break;
+ 
+              case "wednesday":
+                setWednesdayShifts(data);
+                break;
+ 
+              case "thursday":
+                setThursdayShifts(data);
+                break;
+ 
+              case "friday":
+                setFridayShifts(data);
+                break;
+ 
+              case "saturday":
+                setSaturdayShifts(data);
+                break;
+ 
+              case "sunday":
+                setSundayShifts(data);
+                break;
             }
         })
       )
@@ -49,56 +93,135 @@ function App() {
       })
     }
 
-    fetchData("http://localhost:8000/schedule/shifts/");
-    fetchData("http://localhost:8000/schedule/dates/");
-
+    let arr = ["shifts", "dates", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+    
+    // fetch data for each item
+    arr.forEach((item) =>
+      fetchData(item)
+    )
     
 
     // object.keys returns ["Monday", "Tuesday", ... ]
     // forEach iterates over that list.
     // the "day" var represents an individual day JSON object
-    Object.keys(shifts).forEach((day) => {
-      console.log("DAY", day);
+    // Object.keys(shifts).forEach((day) => {
+      // console.log("DAY", day);
       // then you can map the actual shifts in the day objects
-    });
+    // });
 
   }, [])
   
   return (
     <div className="App">
-
-      {shifts?.map(day =>
-        <p>day</p>
-      )}
-
-      <table>
-        <thead>
+      <table className="min-w-full">
+        <thead className="border-b">
             <tr>
-              <th>Monday</th>
-              <th>Tuesday</th>
-              <th>Wednesday</th>
-              <th>Thursday</th>
-              <th>Friday</th>
-              <th>Saturday</th>
-              <th>Sunday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Monday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Tuesday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Wednesday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Thursday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Friday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Saturday</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">Sunday</th>
             </tr>
             <tr>
-              <td>{dates[0]}</td>
-              <td>{dates[1]}</td>
-              <td>{dates[2]}</td>
-              <td>{dates[3]}</td>
-              <td>{dates[4]}</td>
-              <td>{dates[5]}</td>
-              <td>{dates[6]}</td>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[0]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[1]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[2]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[3]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[4]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[5]}</th>
+              <th scope="col" className="text-lg font-medium text-gray-900 px-6 py-4 text-center">{dates[6]}</th>
             </tr>
         </thead>
 
-        <tbody>
-          {/* 1 row of all the shifts for each day */}
+        <tbody className="text-center space-y-4">
           <tr>
-            {
-              
-            }
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {mondayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {tuesdayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {wednesdayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {thursdayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {fridayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {saturdayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+            
+            <td className="text-lg text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {sundayShifts.map((shift, index)=>
+                <div key={index}>
+                  <br/>
+                  <p>{shift['name']}</p>
+                  <p>{shift['position']}</p>
+                  <p>{shift['start_time'] + "-" + shift['end_time']}</p>
+                  <p>{" (" + shift['total_hours'] + " hours )"}</p>
+                </div>
+              )}
+            </td>
+
           </tr>
 
         </tbody>
