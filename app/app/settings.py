9cac126ee6f59f993f821caec231a6a41dbf371c
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2023-01-02 12:22:49 -0500

    debug off for production

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ with open(os.path.join(BASE_DIR, '../../secret_key_small_business_schedule_softw
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
