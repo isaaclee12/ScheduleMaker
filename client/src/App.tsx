ffd807c3f46a1cf45a6417d53363b81115df9e5c
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2023-01-30 18:41:43 -0500

    Pruned away calendar.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,10 +11,6 @@ import Footer from './components/Footer';
 
 import { defaultState, ScheduleContext } from './contexts/ScheduleContext';
 
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
-
 function App() {
   const [requestType, setRequestType] = useState(defaultState.requestType);
   const changeRequestType = (r: string) => {
@@ -26,9 +22,6 @@ function App() {
   };
 
   const serverAddress = defaultState.serverAddress;
-
-  const [calendarData, setCalendarData] = useState<calendarEventArray>([]);
-
 
   interface Shift {
     date: string,
@@ -42,58 +35,9 @@ function App() {
     total_hours: number,
   }
 
-  type ShiftArray = Shift[];
-
-  interface calendarEvent {
-    title: string,
-    date: string
-  }
-
-  type calendarEventArray = calendarEvent[];
-
-  const aggregateShiftsData = (data: ShiftArray[]) => {
-    const allShifts: calendarEventArray = [];
-    let title: string;
-    let date: string;
-    data.forEach(item => {
-      item.forEach(row => {
-        title = row.name.concat("\n", row.position, "\n", row.location, "\n", row.start_time, "-", row.end_time, "\n(", row.total_hours.toString(), " hours)")
-        date = row.date;
-        console.log({title, date});
-        allShifts.push({title, date});
-      });
-    })
-    setCalendarData(allShifts);
-  }
-
-  const handleEventClick = (arg: any) => {
-    alert(arg.event.title);
-  }
-
-  useEffect(() => {
-    fetch("http://" + serverAddress + ":8000/schedule/shifts/", {
-      method: "GET",
-      mode: 'cors'
-    }).then(response => response.json()
-      .then(data => {
-        aggregateShiftsData(data.slice(1, data.length));
-      })
-    )
-
-  }, [])
-
   return (
     <div className="App">
       <NavBar />
-      <div className="p-32">{/* className="flex items-stretch w-screen justify-center h-screen"> */}
-        <FullCalendar 
-          plugins={[dayGridPlugin, interactionPlugin]} 
-          eventClick={handleEventClick}
-          initialView="dayGridMonth" 
-          eventMinHeight={100}
-          eventDisplay={'block'}
-          events={calendarData}/>
-      </div>
       <ScheduleContext.Provider value={{ requestType, changeRequestType, formBackgroundStyle, changeFormBackgroundStyle, serverAddress }}>
         <Routes>
           <Route index element={<Schedule />}></Route>
